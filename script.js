6cfc274fbef01d3a55def9d11d82942ca3145f3f
bhoomika13032007-lgtm <bhoomika13032007@gmail.com>
2025-09-10 21:46:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').onsubmit = function(event) {
-  event.preventDefault();
-  document.getElementById('response').textContent = "Thank you for your message!";
-  this.reset();
-};
